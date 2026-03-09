90cdada480bf40f568ed0e72469e6b3d224cbcda
Jeidervelasquez <jeidervelasquez12@gmail.com>
2026-03-09 15:16:24 -0400

    Refactor enviar_aviso function and add default message

--- bot_recordatorios.py
+++ bot_recordatorios.py
@@ -5,29 +5,28 @@ import requests
 TOKEN_TELEGRAM = "8682305104:AAEJVSbX2uvBH2qXcRqtiMpfpBy4_2n42XY"
 ID_GRUPO_KYSEN = "-5114492594"
 
-def enviar_aviso():
-    mensaje = "⚠️ *¡RECORDATORIO KYSEN!* ⚠️\n\nGuerreros, revisen la App de gestión. Confirmen sus roles, asistencias a eventos y mantengan la disciplina. ¡El clan los necesita!"
-    url = f"https://api.telegram.org/bot{TOKEN_TELEGRAM}/sendMessage"
-    payload = {"chat_id": ID_GRUPO_KYSEN, "text": mensaje, "parse_mode": "Markdown"}
+def enviar_aviso(mensaje="⚠️ *¡RECORDATORIO KYSEN!* ⚠️\n\nGuerreros, revisa la App de gestión. Confirmen sus roles, asistencias a eventos y mantengan la disciplina. ¡El clan los necesita!"):
+    # Asegúrate de tener tu TOKEN y ID arriba de esto
+    URL = f"https://api.telegram.org/bot{TOKEN_TELEGRAM}/sendMessage"
+    carga_util = {"chat_id": ID_GRUPO_KYSEN, "text": mensaje, "parse_mode": "Markdown"}
     
     try:
-        requests.post(url, data=payload)
+        requests.post(URL, data=carga_util)
         print("Recordatorio enviado con éxito al grupo de Telegram.")
     except Exception as e:
         print(f"Error al enviar: {e}")
 
-# Aquí ajustas las horas a las que quieres que se envíe el mensaje a tu equipo
+# Aquí ajustas las horas a las que quieres que se envíe el mensaje
 schedule.every().day.at("09:00").do(enviar_aviso)
 schedule.every().day.at("14:30").do(enviar_aviso)
 schedule.every().day.at("20:00").do(enviar_aviso)
 
 print("Iniciando motor de alarmas Kysen E-Sports. Presiona Ctrl+C para detenerlo.")
 
-# Agregar esto para probar que sí conecta con Telegram
+# Mensaje de prueba de vida
 print("Iniciando sistema... Enviando mensaje de prueba.")
-enviar_notificacion_tg("🤖 *SISTEMA KYSEN:* El bot de recordatorios acaba de ser encendido y está vigilando los eventos.")
+enviar_aviso("🤖 *SISTEMA KYSEN:* El bot de recordatorios acaba de ser encendido y está vigilando los eventos.")
+
 while True:
-
     schedule.run_pending()
-
     time.sleep(60) # Revisa cada minuto para no gastar recursos
